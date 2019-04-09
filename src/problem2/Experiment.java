59ba4bc3571b6e8491373c918f214f81c887d0fd
Zixuan Guo <i@zxguo.me>
2019-04-09 15:29:55 -0500

    final

--- src/problem2/Experiment.java
+++ src/problem2/Experiment.java
@@ -1,6 +1,7 @@
 package problem2;
 
 import java.util.Arrays;
+import java.util.Random;
 
 /**
  * A quick-and-dirty experiment that lets us sort values presented on
@@ -11,12 +12,37 @@ import java.util.Arrays;
 public class Experiment {
 
   public static void main(String[] args) throws Exception {
-    int[] values = new int[args.length];
-    for (int i = 0; i < args.length; i++) {
-      values[i] = Integer.parseInt(args[i]);
-    } // for
-    QuicksortDNF.sort(values);
-    System.out.println(Arrays.toString(values));
+    Random rand = new Random();
+    for (int i = 1; i < 100; i++) {
+      int[] arr1 = new int[i];
+      int[] arr2 = new int[i];
+      for (int j = 0; j < arr1.length; j++) {
+        int randNum = rand.nextInt(100);
+        arr1[j] = randNum;
+        arr2[j] = randNum;
+      }
+      QuicksortDNF.sort(arr1);
+      Arrays.sort(arr2);
+      if (!compare(arr1, arr2)) {
+        System.out.println("Error at: " + i);
+        System.out.println(Arrays.toString(arr1));
+        System.out.println(Arrays.toString(arr2));
+      }
+    }
   } // main(Sting[])
+
+  private static boolean compare(int[] arr1, int[] arr2) {
+    if (arr1.length != arr2.length) {
+      return false;
+    }
+
+    for (int i = 0; i < arr1.length; i++) {
+      if (arr1[i] != arr2[i]) {
+        return false;
+      }
+    }
+
+    return true;
+  }
 
 } // Experiment
--- src/problem2/QuicksortDNF.java
+++ src/problem2/QuicksortDNF.java
@@ -1,5 +1,7 @@
 package problem2;
 
+import java.util.Random;
+
 /**
  * An implementation of Quicksort that partitions using the Dutch National
  * Flag algorithm.
@@ -12,6 +14,7 @@ public class QuicksortDNF {
    * Sort an array of integers in place, using Quicksort w/DNF.
    */
   public static void sort(int[] values) {
+    shuffle(values);
     sort(values, 0, values.length);
   } // sort(int[])
   
@@ -19,31 +22,46 @@ public class QuicksortDNF {
    * Sort the subarray of values between lb (inclusive) and ub (exclusive)
    * using Quicksort w/DNF
    */
-  private static void sort(int[] values, int lb, int ub) {
+  private static void sort(int[] values, int low, int high) {
     // Special case: 0 or 1 elements
-    if (ub - lb <= 1) {
+    if (high - low <= 1) {
       return;
     } // if zero or one elements
     
-    // Prepare bounds for DNF
-    int r = 0;      // The end of the red/small section
-    int w = 0;      // The end of the white/equal section
-    int b = ub;     // The start of the blue/greater section
-    
-    // Rearrange the values
-    swap(values, lb, w);   // Incorrect!
-    
+    int lb = low;
+    int hb = high;
+    int pos = lb + 1;
+    int pivot = values[low];
+
+    while (pos < hb) {
+      if (values[pos] < pivot) {
+        swap(values, lb++, pos++);
+      } else if (values[pos] > pivot) {
+        swap(values, pos, --hb);
+      } else {
+        pos++;
+      }
+    }
+
     // Recurse
-    sort(values, lb, r);
-    sort(values, b, ub);
+    sort(values, low, lb);
+    sort(values, hb, high);
   } // sort(int[], int, int)
   
   /**
    * Swap the elements at positions i and j.
    */
   private static void swap(int[] values, int i, int j) {
+    if (i == j) { return; } // no need for swap if same indexes
     int tmp = values[i];
     values[i] = values[j];
     values[j] = tmp;
   } // swap(int[], int, int)
+
+  private static void shuffle(int[] array) {
+    Random rand = new Random();
+    for (int i = array.length - 1; i >= 0; i--) {
+      swap(array, i, rand.nextInt(i + 1));
+    }
+  }
 } // QuicksortDNF
